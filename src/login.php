18f1d945101f544bb9e1a6447b04fbd057a5c392
Shadi Jiha <shadijiha@gmail.com>
2020-07-17 19:35:38 -0400

    Added footer

--- src/login.php
+++ src/login.php
@@ -33,9 +33,7 @@ require_once('php/config.php');
 				</span>
 			</button>
 		</a>
-	</div>
-
-        
+	</div>        
 		<div class="login_form" style="font-family: 'Roboto';">
 			<br />
 			<br />
@@ -70,5 +68,34 @@ require_once('php/config.php');
 				</form>	
 			</center>
 		</div>
-        </body>
-        </html>
+
+		<!-- FOOTER HERE -->
+		<div id="footer">
+			<center>
+			<table>
+
+				<tr></tr><tr></tr><tr></tr><tr></tr><tr></tr>
+				<td></td><td></td><td></td><td></td><td></td><td></td>
+
+			<th><h3 style="color:white; text-align:center;">Caliprex<h3>					
+			<tr> 
+					<th><h3 style="color:white; font-style:robotto;"> <br>Subscribe to our Newsletter!</h3>
+					<td></td>
+					<td></td>
+					<td></td>
+					<th> <input type="text" style="height:30px;font-size:20;width:200px;"placeholder="Email address">
+					<th> <input type="submit" id="btn_work" class="btn" size="20"; value="GO">
+					<td><pre>	</pre></td>
+					<th><a href="https://www.facebook.com/Caliprex-121401789649042" target="_blank"><image src="../assets/Icons/facebook.png" alt="Facebook image"style="float:right; margin-right:0.5em" width="50" height="35"></a>
+					<th><a href="https://www.instagram.com/caliprex/" target="_blank"><image src="../assets/Icons/instagram.png" alt="Instagram image" style="float:right; margin-right:0.5em" width="50" height="35"></a>
+					<th><a href="https://twitter.com/caliprex" target="_blank"><image src="../assets/Icons/twitter.png" alt="Twitter image" style="float:right; margin-right:0.5em" width="50" height="35"></a>
+					<th><a href="https://Pintrest.com/caliprex" target="_blank"><image src="../assets/Icons/pinterest.png" alt="Pintrest image" style="float:right; margin-right:0.5em" width="50" height="35"></a>
+					<th><a href="https://www.youtube.com/channel/UCvZRW67axwzk6fw5dBSw-iQ?view_as=subscriber" target="_blank"><image src="../assets/Icons/youtube.png" alt="Youtube image" style="float:right; margin-right:0.5em" width="50" height="35"></a>
+					<th><a href="contactus.php" style="color:white;"><h3>About Us |</a></h3>      
+					<th><a href="login.php" ><h3 style="color:white;">Login</h3></a>
+			</tr>
+			</table>
+			</center>
+		</div>
+    </body>
+</html>
