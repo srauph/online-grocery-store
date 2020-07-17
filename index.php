eb0be90e3984898aef114bd467c5bef817663862
Shadi Jiha <shadijiha@gmail.com>
2020-07-16 23:05:56 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
-<?php
+<html>
+    <head>
 
-echo 'this is a test'
-
-?>
+    <script>
+    
+    window.onload = function()  {
+        window.location.href = "src/index.php"    
+    }
+    
+    </script>
+    </head>
+    <body>
+    </body>
+</html>
