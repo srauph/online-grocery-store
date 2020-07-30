bfe7be51e02b13e19588706a15a9641db3f502b8
Shadi Jiha <shadijiha@gmail.com>
2020-07-29 21:18:03 -0400

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,20 @@
  *
  */
 
-let cart;
+let cart = new Cart();
 
 window.onload = function () {
 	//Initialize Objects
-	cart = new Cart();
+	cart = ;
+};
+
+window.onbeforeunload = function (e) {
+	window.onunload = function () {
+		window.localStorage.isMySessionActive = "false";
+	};
+	return undefined;
+};
+
+window.onload = function () {
+	window.localStorage.isMySessionActive = "true";
 };
