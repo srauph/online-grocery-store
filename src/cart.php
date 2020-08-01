457dd11350253d75974e62a878aef785cf66c0f5
srauph <89359211+srauph@users.noreply.github.com>
2020-08-01 17:47:13 -0400

    fixed cart css a little bit

--- src/cart.php
+++ src/cart.php
@@ -2,6 +2,7 @@
 
 <head>
     <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/cart.css">
     <title>Cart Summary page</title>
     <style>
     input {
@@ -12,7 +13,7 @@
         text-align: center;
     }
 
-    :-moz-placeholder {
+    ::-moz-placeholder {
         text-align: center;
     }
     </style>
@@ -36,7 +37,9 @@
         const DOM = document.getElementById("__cart_content_table");
 
         if (items == null || items.length == 0) {
-            DOM.innerHTML = "Cart is empty";
+            //document.getElementById("___cart_content_table_r2").innerHTML = "<br><br>Cart is empty.";
+            DOM.innerHTML = "<h2>Cart is empty.</h2> Let's add some stuff to this!";
+            
             // Write the GST and QST
             document.getElementById("qst").innerHTML = "$" + (0).toFixed(2);
             document.getElementById("gst").innerHTML = "$" + (0).toFixed(2);
@@ -50,15 +53,26 @@
         for (const item of items) {
 
             DOM.innerHTML +=
-                `<tr>
+                `<tr class="cart_list">
                     <td style = "text-align:center">
-                        <img src = "../assets/Images/${item.image}" alt = "" width = "150" height = "150" ></td>
-                        <td width = "100" style = "text-align:center" ><h2>${item.name}</h2> 
+                        <img src = "../assets/Images/${item.image}" alt = "" width = "150" height = "150" >
+                    </td>
+                    <td style = "text-align:center" >
+                        <div><h2>${item.name}</h2></div>
                     </td>
                     <td style="text-align:center;" > 
-                        <input type="number" style="text-align:center; width:90; height:45;" placeholder = "QUANTITY" /> 
+                        <div class="cart_qty_selector">
+                            <button type="submit" class="cart_plus_minus_btn" onclick="updateQty(false, 20);">-</button>
+                            <input id="productQty" type="text" class="cart_qty" value="0" readonly></input>
+                            <button type="submit" class="cart_plus_minus_btn" onclick="updateQty(true, 20);">+</button>
+                        </div>
                     </td>                     
-                     <td style = "text-align:right" ><h2> $${item.cost} </h2> </td> 
+                    <td style = "text-align:center" >
+                        <div><h2> $${item.cost} </h2></div> 
+                    </td>
+                    <td style = "text-align:center" >
+                        <input type="button" class="cart_remove_btn" onclick="" value="Remove Item" /> 
+                    </td> 
                 </tr>`;
 
             totalPrice += item.cost;
@@ -73,10 +87,15 @@
     document.addEventListener("DOMContentLoaded", function() {
         init();
     });
+
+    function clearCart() {
+        localStorage.removeItem("cart");
+        init();
+    }
     </script>
 </head>
 
-<body style="background-color:lightgrey;">
+<body>
     <div id="__top_banner">
         <a class="white" href="login.php" title="Login to your account">Login</a>
         |
@@ -121,201 +140,128 @@
             </ul>
         </div>
     </div>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <div class="cart_left">
-        <div class="border4">
-            <table style="width:100%" id="__cart_content_table">
+
+    <h1 style="font-size:48; padding:2%; text-align:center; background-color:white;">Cart</h1>
+
+    <div class="cart_grid">
+        <div class="cart_items">
+            <table id="__cart_content_table">
                 <tr id="___init">
-                    <th height="100" width="360" style="text-align:right">
-                        <h2 class="grey">Product Image</h2>
+                    <th>
+                        <div><h2 class="grey">Product Image</h2></div>
                     </th>
 
-                    <th height="100" width="360" style="text-align:right">
-                        <h2 class="grey">Product name</h2>
+                    <th>
+                        <div><h2 class="grey">Product name</h2></div>
                     </th>
 
-                    <th height="100" width="360" style="text-align:right">
-                        <h2 class="grey">Quantity</h2>
+                    <th>
+                        <div><h2 class="grey">Quantity</h2></div>
                     </th>
 
-                    <th style="text-align:right">
-                        <h2 class="grey">Total Price</h2>
+                    <th>
+                        <div><h2 class="grey">Total Price</h2></div>
                     </th>
                     <td>
-                        <input type="button" onclick="localStorage.clear(); init();" value="Clear All" />
+                        <input type="button" class="cart_btn" style="font-size: 16px;" onclick="clearCart();" value="Clear All" />
                     </td>
                 </tr>
-                <tr>
-
-                    <td style="text-align:center">
-                        <image src="../assets/Images/cheetos.jpg" alt="cheetos image" width="150" height="150">
-                    </td>
-                    <td width="100" style="text-align:center">
-                        <h2>Cheetos</h2>
-                    </td>
-
-                    <td style="text-align:center;"><input type="number" style="text-align:center; width:90; height:45;"
-                            placeholder="QUANTITY" /></td>
-                    <td style="text-align:center">
-                        <h2>$5.99</h2>
-                    </td>
-                </tr>
-                <tr>
-                    <td style="text-align:center">
-                        <image src="../assets/Images/frozenfries.jpg" alt="cheetos image" width="150" height="150">
-                    </td>
-                    <td width="100" style="text-align:center">
-                        <h2>Frozen Fries</h2>
-                    </td>
-
-                    <td style="text-align:center;"><input type="number" style="text-align:center; width:90; height:45;"
-                            placeholder="QUANTITY" /></td>
-                    <td style="text-align:right">
-                        <h2>$15.98</h2>
-                    </td>
-                </tr>
+                <!-- <tr><td id="___cart_content_table_r2" colspan="5"></td></tr> -->
             </table>
         </div>
-    </div>
-    <div class="cart_right">
-        <div class="border1">
-            <h1>CART SUMMARY</h1>
-            <label class="name">Estimated total</label><br>
-            <input type="text" style="height:80px; font-size:40; width:300;" placeholder="$0.00"><br><br>
+        <div class="cart_right">
+            <div class="border">
+                <h1>CART SUMMARY</h1>
+                <label class="name">Estimated total</label><br>
+                <input type="text" style="height:80px; font-size:40; width:300;" placeholder="$0.00"><br><br>
 
-            <input type="submit" class="btn" style=width:300; size="20" ; placeholder="PLACE ORDER" value="PLACE ORDER">
+                <input type="submit" class="btn" style=width:300; size="20" ; placeholder="PLACE ORDER" value="PLACE ORDER">
 
-            <pre><h3 class="red" style= "font-size:17;" >     Minimum $45.00 order.    </h3></pre>
-            <table>
-                <tr>
-                    <td>
-                        <pre><h3 class="black" style="font-size:17;">Number of items:   </h3></pre>
-                    <td>
-                    <td>
-                    <td>
-                    <td>
-                    <td>
-                    <td>
-                    <td>
-                    <td>
-                        <h3>0 </h3>
-                </tr>
-                <tr>
-                    <td>
-                        <h3 class="black" style="font-size:17;">QST:</h3>
-                    <td>
-                    <td>
-                    <td>
-                    <td>
-                    <td>
-                    <td>
-                    <td>
-                    <td>
-                        <h3 id="qst">$0.00</h3>
-                </tr>
-                <tr>
-                    <td>
-                        <h3 class="black" style="font-size:17;">GST:</h3>
-                    <td>
-                    <td>
-                    <td>
-                    <td>
-                    <td>
-                    <td>
-                    <td>
-                    <td>
-                        <h3 id="gst">$0.00<h3>
-                </tr>
-                <tr>
-                    <td>
-                        <h3 class="black" style="font-size:17;">Total:</h3>
-                    <td>
-                    <td>
-                    <td>
-                    <td>
-                    <td>
-                    <td>
-                    <td>
-                    <td>
-                        <h3 id="total">$0.00</h3>
+                <pre><h3 class="red" style= "font-size:17;" >     Minimum $45.00 order.    </h3></pre>
 
-                </tr>
-            </table>
-
-
-        </div>
-
-        <div class="border2">
-            <h1 style="font-size:26;"> PROMOTIONAL CODE<h1>
-                    <table>
-                        <tr>
-                            <th><label class="name">Apply a promotional code</label>
-                            <th>
-                                <image src="../assets/Icons/questionmark.png" alt="question mark icon"
-                                    style="float:right; margin-right:0.5em" width="50" height="25">
-                        </tr>
-                    </table>
-                    <table>
-                        <tr>
-                            <th><input type="text" style="height:60px;font-size:10;width:200px;"
-                                    placeholder="EX: PROMCODE1     ">
-                            <th><input type="submit" style="height:60; text-align:center; font-size:10;" class="btn"
-                                    value="APPLY">
-                        </tr>
-                    </table>
-        </div>
-        <div class="border1">
-            <center>
-                <h2>Delivery</h2>
-            </center>
-            <h4>Caliprix Atwater</h4>
-            <p>1150 Maisonneuve BLVD W, H3A 1M7 , Montreal , Canada</p>
-            <p>(514) 553-4360</p>
-            <p>Thursday: 07:00 - 22:00</p>
-        </div>
-        <div class="border3">
-            <h4> To ensure accurate product avaliability and pricing, please choose your store.</h4>
-            <center>
-                <table border="1">
+                <table style=width:100%>
                     <tr>
-                        <th> <input type="text" style="height:60px;font-size:15;width:200px; "
-                                placeholder="Our only location H3A 1M7"></th>
-                        <th><a href="https://www.google.com/maps/place/1150+Maisonneuve+Blvd+W,+Montreal,+QC/@45.5000431,-73.5778952,17.18z/data=!4m5!3m4!1s0x4cc91a416d27dcf1:0xe258368f00acb82c!8m2!3d45.5002825!4d-73.5749175"
-                                target="_blank">
-                                <image src="../assets/Icons/magnifyingglass.png" alt="A magnifying glass image"
-                                    style="float:right; margin-right:0.5em" width="50" height="25">
-                        </th>
-                        </a>
+                        <td><h3 class="black" style="font-size:17;">Number of items:</h3></td>
+                        <td style="text-align:right;"><h3>0</h3></td>
+                    </tr>
+                    <tr>
+                        <td><h3 class="black" style="font-size:17;">QST:</h3></td>
+                        <td style="text-align:right;"><h3 id="qst">$0.00</h3></td>
+                    </tr>
+                    <tr>
+                        <td><h3 class="black" style="font-size:17;">GST:</h3></td>
+                        <td style="text-align:right;"><h3 id="gst">$0.00<h3></td>
+                    </tr>
+                    <tr>
+                        <td><h3 class="black" style="font-size:17;">Total:</h3></td>
+                        <td style="text-align:right;"><h3 id="total">$0.00</h3></td>
                     </tr>
                 </table>
-                <br>
+            </div>
 
-                <<a href="login.php"><input type="submit" class="btn" style="width:300; size=20;"
-                        value="MY POSITION"></a>
-            </center>
+            <div class="border">
+                <h1 style="font-size:26;"> PROMOTIONAL CODE<h1>
+                        <table style="width:100%;">
+                            <tr>
+                                <td><label class="name">Apply a promotional code</label></td>
+                                <td style="text-align:right;">
+                                    <image src="../assets/Icons/questionmark.png" alt="question mark icon"
+                                        width="50" height="25"></image>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><input type="text" style="height:60px;font-size:10;width:200px;"
+                                        placeholder="EX: PROMCODE1     "></td>
+                                <td style="text-align:right;"><input type="submit" style="height:60; text-align:center; font-size:10;" class="btn"
+                                        value="APPLY"></td>
+                            </tr>
+                        </table>
+            </div>
+
+            <div class="border">
+                <h2>Delivery</h2>
+                <h4>Caliprix Atwater</h4>
+                <p>1150 Maisonneuve BLVD W, H3A 1M7 , Montreal , Canada</p>
+                <p>(514) 553-4360</p>
+                <p>Thursday: 07:00 - 22:00</p>
+            </div>
+            <div class="border">
+                <h4> To ensure accurate product avaliability and pricing, please choose your store.</h4>
+                <center>
+                    <!-- <table border="1"> -->
+                    <table style="border:1;">
+                        <tr>
+                            <th> <input type="text" style="height:60px;font-size:15;width:200px; "
+                                    placeholder="Our only location H3A 1M7"></th>
+                            <th><a href="https://www.google.com/maps/place/1150+Maisonneuve+Blvd+W,+Montreal,+QC/@45.5000431,-73.5778952,17.18z/data=!4m5!3m4!1s0x4cc91a416d27dcf1:0xe258368f00acb82c!8m2!3d45.5002825!4d-73.5749175"
+                                    target="_blank">
+                                    <image src="../assets/Icons/magnifyingglass.png" alt="A magnifying glass image"
+                                        style="float:right; margin-right:0.5em" width="50" height="25">
+                            </th>
+                            </a>
+                        </tr>
+                    </table>
+                    <br>
+
+                    <<a href="login.php"><input type="submit" class="btn" style="width:300; size=20;"
+                            value="MY POSITION"></a>
+                </center>
 
 
-            <h4> Please log in or create an account to reserve your timeslot</h4>
-            <a href="login.php">
-                <button type="submit" class="btn" style=width:300; size="20">LOGIN</button><br><br>
-            </a>
-            <a href="register.php"><button type="submit" class="btn" style=width:300; size="20" ;>CREATE AN
-                    ACCOUNT</button><br><br>
-            </a>
+                <h4> Please log in or create an account to reserve your timeslot</h4>
+                <a href="login.php">
+                    <button type="submit" class="btn" style=width:300; size="20">LOGIN</button><br><br>
+                </a>
+                <a href="register.php"><button type="submit" class="btn" style=width:300; size="20" ;>CREATE AN
+                        ACCOUNT</button><br><br>
+                </a>
+            </div>
         </div>
-
-
-
-
     </div>
+
+    <br />
+    <br />
+
     <div id="footer">
         <center>
             <table>
