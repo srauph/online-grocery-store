9e0a5158d590bfaf667384f52e3bc7f9d2a1c8e4
srauph <89359211+srauph@users.noreply.github.com>
2020-08-02 15:52:59 -0400

    fixed mango's stuff

--- src/apple.php
+++ src/apple.php
@@ -3,7 +3,7 @@
 <head>
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <link rel="stylesheet" type="text/css" href="css/aisle_beverage.css">
-    <title>Apples</title>
+    <title id="productTitle">Apple</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <script type="text/javascript" src="scripts/Util.js"></script>
@@ -12,7 +12,160 @@
     <script type="text/javascript" src="scripts/Sales.js"></script>
     <script type="text/javascript" src="scripts/AbstractComponent.js"></script>
     <script type="text/javascript" src="scripts/main.js"></script>
+    <script type="text/javascript" src="scripts/Fruit.js"></script>
 </head>
+<script>
+
+    /** 
+     * name = "Base" product name
+     * desc = Product descripton
+     * price = Product price
+     * img = Link to product image
+     * options = Number of color options available
+     * optionsSize = Number of size options available
+     * limit = Amount to limit order qty to
+     */
+    name = "Apple";
+    desc = "Apple, meet the world's favourite apple for snacks. The heart-shaped delicious apple has a bright surface. This delicious apple shines in fresh, crisp salads, for its crunchy texture and lightly sweet taste.";
+    price = 1.99;
+    img = "../assets/Images/apple.jpg";
+    options = 2;
+    optionsSize = 2;
+    limit = 20;
+    id = 301;
+
+
+    /** 
+     * itemName = Name to append to `name` based on the color
+     * sizename = Name to append to `name` based on size
+     * currentItem and currentSize are the current color and size options selected, respectively.
+     * showAll = False if description should be collapsed, true if it should be expanded.
+     * qty = Quantity to add to cart.
+     */
+    itemName = "Red";
+    sizeName = "Regular";
+    currentItem = 1;
+    currentSize = 1;
+
+    showAll = false;
+    qty = 0;
+
+    /** 
+     * The next two functions save and load session data to and from the browser
+     * Currently the data only sticks if you refresh or navigate away from the page.
+     * Replace "sessionStorage" with "localStorage" if you want the data to stick after closing the
+     * tab or window as well. (Replace that in both functions).
+     */
+    function saveSessionData() {
+        sessionStorage.appleQty = qty;
+        sessionStorage.appleCurrentItem = currentItem;
+        sessionStorage.appleCurrentSize = currentSize;
+        sessionStorage.appleShowAll = showAll;
+    }
+
+    function loadSessionData() {
+        if (sessionStorage.appleCurrentItem) {
+            currentItem = parseInt(sessionStorage.appleCurrentItem);
+        }
+        if (sessionStorage.appleCurrentSize) {
+            currentSize = parseInt(sessionStorage.appleCurrentSize);
+        }
+        if (sessionStorage.appleQty) {
+            qty = parseInt(sessionStorage.appleQty);
+        }
+        if (sessionStorage.appleShowAll == "true") {
+            document.getElementById("showDescBtn").innerHTML = "Less Description...";
+            showAll = true;
+        }
+
+        // Restore the page to the original state
+        changeProduct(currentItem);
+        setQty(qty);
+        displayDesc();
+
+    changeProductSize(currentItem);
+    setQty(qty);
+    displayDesc();
+
+    }
+
+    /** 
+     * General format for these two functions:
+     * 
+     * For the switch statement: 
+     * sizeName and itemName hold the name you wish to use for the item size and item color, respectively.
+     * In Fruit.js these two names are concatenated with the variable "name" to generate a product title. 
+     * 
+     * If you wish to modify the description or the image with the product size and
+     * color, you need to find some way to mix the type(color) and size. EG for name, sizeName and itemName
+     * are used to get this effect.
+     */
+    function changeProductSize(size){
+
+        switch (size) {
+
+            // Update relevant variables
+
+            case 1: //Regular Apple
+            sizeName = "Regular";
+            // desc = "Apple, meet the world's favourite apple for snacks. The heart-shaped delicious apple has a bright surface. This delicious apple shines in fresh, crisp salads, for its crunchy texture and lightly sweet taste.";
+            price = 1.99;
+            // img = "../assets/Images/apple.jpg";
+            currentSize = 1;
+            updateID();
+            updatePageContents();
+            break;
+
+            case 2: // If the option selected is Mini Apple
+            sizeName = "Mini";
+            // desc = "Apple, meet the world's favourite apple for snacks. The heart-shaped delicious apple has a bright surface. This delicious apple shines in fresh, crisp salads, for its crunchy texture and lightly sweet taste.";
+            price = 0.99;
+            // img = "../assets/Images/apple.jpg";
+            currentSize = 2;
+            updateID();   
+            updatePageContents(); // Ditto.
+            break;
+
+        }
+    }
+
+    function changeProduct(type) {
+
+        switch (type) {
+
+            // Update relevant variables
+
+            case 1: //Red Apple
+            itemName = "Red";
+            // desc = "Apple, meet the world's favourite apple for snacks. The heart-shaped delicious apple has a bright surface. This delicious apple shines in fresh, crisp salads, for its crunchy texture and lightly sweet taste.";
+            // price = 1.99;
+            img = "../assets/Images/redapple.jpg";
+            currentItem = 1;
+            updateID();
+            updatePageContents();
+            break;
+
+            case 2: // If the option selected is Green Apple
+            itemName = "Green";
+            // desc = "Apple, meet the world's favourite apple for snacks. The heart-shaped delicious apple has a bright surface. This delicious apple shines in fresh, crisp salads, for its crunchy texture and lightly sweet taste.";
+            // price = 1.99;
+            img = "../assets/Images/greenapple.jpg";
+            currentItem = 2;
+            updateID();
+            updatePageContents(); // Ditto.
+            break;
+    
+        }
+    }
+
+    /** 
+     * Updates the item ID based on options selected.
+     */
+    function updateID() {
+        id = 300 + 10*currentSize + currentItem;
+    }
+
+</script>
 
 <body>
     <div id="__top_banner">
@@ -49,35 +202,36 @@
     <div class="beverage_page">
 
         <div class="beverage_left">
-            <img src="../assets/Images/apple.jpg" style="width:80%; height=80%;" alt="Apples">
+        <img id="productImg" src="" style="width:80%; height=80%;" alt="" />
         </div>
 
         <div class="beverage_right">
-            <h1 style="font-size:48; padding:2%; background-color:white;">Apple (One apple)</h1><br>
-            <p style="font-size:24px">A soft green apple. Also comes in juicy and hard.</p>
-            <button type="submit" class="product_description_btn">More Description...</button><br><br><br>
+            <!-- Product details -->
+            <h1 id="productName" style="font-size:48;"></h1><br>
+            <p><span id="productPrice" class="product_price"></span></p><br><hr>
+            <p id="productDesc" class="product_desc"></p>
+            <button id="showDescBtn" type="submit" class="product_description_btn" onclick="showHideDesc();">More Description...</button><br><br><br>
+            <!-- Product size -->
             <p>You may choose a different size using the options below...</p>
-            <button type="submit" class="product_option_btn">soft apple</button>
-            <button type="submit" class="product_option_btn">juicy apple</button>
-            <button type="submit" class="product_option_btn">hard apple</button><br><br><br>
-            <button type="submit" class="btn">Add To Cart</button><br><br>
-            <label class="red">Quantity</label>
-            <br>
-            <input type="number" max="10" min="1" style="text-align:center; width:90; height:45;"
-                placeholder="QUANTITY" />
-            <br>
+            <button id="productSizeOption1" type="submit" class="product_option_btn" onclick="changeProductSize(1);">Regular Apple</button>
+            <button id="productSizeOption2" type="submit" class="product_option_btn" onclick="changeProductSize(2);">Mini Apple</button><br><br><br>
+            <!-- Product option selection buttons -->
+            <p>You may choose a different types using the options below...</p>
+            <button id="productOption1" type="submit" class="product_option_btn" onclick="changeProduct(1);">Red Apple</button>
+            <button id="productOption2" type="submit" class="product_option_btn" onclick="changeProduct(2);">Green Apple</button><br><br><br>
+            <!-- Quantity selector and Add to Cart functionality -->
+            <div class="cart_grid">
+                <div class="cart_qty_selector">
+                    <button type="submit" class="cart_plus_minus_btn" onclick="updateQty(false, 20);">-</button>
+                    <input id="productQty" type="text" class="cart_qty" value="0" readonly></input>
+                    <button type="submit" class="cart_plus_minus_btn" onclick="updateQty(true, 20);">+</button>
+                </div>
+                <div id="productMax" class="cart_qty_max_msg">
+                    Quantity Limit: 20
+                </div>
+                <button type="submit" class="cart_btn" onclick="addToCart();">Add To Cart</button>
+            </div>
         </div>
-    </div>
-
-
-
-
-
-
-
-
-
-
     <br />
     <br />
     </div>
---
+++ src/scripts/Fruit.js
@@ -0,0 +1,163 @@
+/** 
+ * name:    Name of the object. Used in the page title, the item title, and as the alt image text
+ * desc:    Full description of the object.
+ * price:   Price of the object
+ * img:     Link to the object image file
+ * options: Amount of different options available (eg different product sizes)
+ */
+var name = "";
+var desc = "";
+var price = 0.0;
+var img = "";
+var options = 1;
+var optionsSize = 1;
+var id = 0;
+var limit = 1;
+
+/**
+ * showAll:     When true, show the product description in its entirety.
+ * currentItem: The currently selected product option (eg product size, etc).
+ * qty:         Quantity to add to cart.
+ */
+var showAll = false;
+var itemName = "Red";
+var sizeName = "Regular";
+var currentItem = 1;
+var currentSize = 1;
+var qty = 0;
+
+/**
+ * This function updates the page contents to reflect changes when a different product option
+ * is selected.
+ */
+function updatePageContents() {
+    document.getElementById("productTitle").innerHTML = sizeName + " " + itemName + " " + name; // Page title
+    document.getElementById("productImg").src = img; // Product image
+    document.getElementById("productImg").alt = name; // Product image alt text
+    document.getElementById("productName").innerHTML = sizeName + " " + itemName + " " + name; // Product title
+    document.getElementById("productPrice").innerHTML = "$" + price + " (In stock!)"; // Product price
+    document.getElementById("productMax").innerHTML = "Quantity Limit: " + limit
+    if (qty > limit) {
+        setQty(limit);
+    }
+    changeSelectionBtn(currentItem); // Product option buttons
+    changeSizeBtn(currentSize); // Product option buttons
+    displayDesc(); // Product description
+}
+
+/**
+ * This function updates the contents of the description according to the product option, and
+ * expands (or shortens) it when requested.
+ */
+function displayDesc() {
+    if (showAll) {
+        document.getElementById("productDesc").innerHTML = desc;
+    } else {
+        document.getElementById("productDesc").innerHTML = Sales.private_string_reduceChars(desc, 41);
+    }
+}
+
+/**
+ * This function toggles whether the descroption should be expanded or collapsed.
+ */
+function showHideDesc() {
+    if (showAll) {
+        document.getElementById("showDescBtn").innerHTML = "More Description...";
+        showAll = false;
+        displayDesc();
+    } else {
+        document.getElementById("showDescBtn").innerHTML = "Less Description...";
+        showAll = true;
+        displayDesc();
+    }
+}
+
+/**
+ * This function updates the appearance of the buttons to reflect changes to which product option
+ * is currently selected. (For apple.php this is color)
+ * @param {*} productOption The product option that was selected (For apple.php this is color)
+ */
+function changeSelectionBtn(productOption) {
+    for (let i = 1; i <= options; i++) {
+        document.getElementById("productOption" + i).className = "product_option_btn";
+    }
+    document.getElementById("productOption" + productOption).className = "product_option_btn_selected";
+}
+
+/**
+ * This function updates the appearance of the buttons to reflect changes to which product size option
+ * is currently selected.
+ * @param {*} productSizeOption The product size option that was selected
+ */
+function changeSizeBtn(productSizeOption) {
+    for (let i = 1; i <= optionsSize; i++) {
+        document.getElementById("productSizeOption" + i).className = "product_option_btn";
+    }
+    document.getElementById("productSizeOption" + productSizeOption).className = "product_option_btn_selected";
+}
+
+
+
+/**
+ * Changes the quantity to add to cart
+ * @param {*} newQty Quantity value to use.
+ */
+function setQty(newQty) {
+    qty = newQty;
+    document.getElementById("productQty").value = newQty;
+}
+
+/**
+ * Updates the quantity to add to cart
+ * @param {*} direction When true, increment qty. Else, decrement qty.
+ */
+function updateQty(direction) {
+
+    qty = document.getElementById("productQty").value;
+
+    if (qty < 0) {
+        setQty(0);
+        return;
+    } else if (qty > limit) {
+        alert("Warning: Maximum item purchase limit exceeded. Reverting to " + limit + ".");
+        setQty(limit);
+        return;
+    }
+
+    if (direction) {
+        if (qty == limit) {
+            return;
+        } else {
+            qty++;
+            document.getElementById("productQty").value = qty;
+        }
+    } else {
+        if (qty == 0) {
+            return;
+        } else {
+            qty--;
+            document.getElementById("productQty").value = qty;
+        }
+    }
+}
+
+/**
+ * This function adds the selected quantity of the selected product to the cart.
+ */
+function addToCart() {
+    qty = document.getElementById("productQty").value;
+
+    if (qty <= 0) {
+        alert("You need to indicate a quantity before adding to cart!");
+        return;
+    } else if (qty > limit) {
+        alert("Warning: Maximum item purchase limit exceeded. Reverting to " + limit + ".");
+        qty = limit;
+        document.getElementById("productQty").value = limit;
+        return;
+    } else {
+        // for (let i = 0; i < qty; i++) {
+        cart.void_add(new Item(id, name, currentItem, img.substring(17), price, parseInt(qty), limit, 0, ''));
+        // }
+    }
+}
