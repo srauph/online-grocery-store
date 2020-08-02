b5c01c276559bd7d8c7ea2799a4384180c627c0a
srauph <89359211+srauph@users.noreply.github.com>
2020-08-02 17:00:29 -0400

    fixed updater bug on cart.php page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,24 +6,24 @@
 let cart;
 let cart_top_right_updater;
 
-window.onload = function () {
-	//Initialize Objects
-	if (JSON.parse(localStorage.getItem("cart")) == null) {
-		cart = new Cart();
-	} else {
-		cart = new Cart(JSON.parse(localStorage.getItem("cart")));
-	}
+window.onload = function() {
+    //Initialize Objects
+    if (JSON.parse(localStorage.getItem("cart")) == null) {
+        cart = new Cart();
+    } else {
+        cart = new Cart(JSON.parse(localStorage.getItem("cart")));
+    }
 
-	cart.private_void_updateValue();
-	cart_top_right_updater = setInterval(function () {
-		cart.private_void_updateValue();
-	}, 1000);
+    cart.private_void_updateValue();
+    // cart_top_right_updater = setInterval(function() {
+    //     cart.private_void_updateValue();
+    // }, 1000);
 
-	if (typeof loadSessionData === "function") loadSessionData();
+    if (typeof loadSessionData === "function") loadSessionData();
 };
 
-window.onunload = function () {
-	if (typeof saveSessionData === "function") saveSessionData();
+window.onunload = function() {
+    if (typeof saveSessionData === "function") saveSessionData();
 
-	clearInterval(cart_top_right_updater);
+    clearInterval(cart_top_right_updater);
 };
