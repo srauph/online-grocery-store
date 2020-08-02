9c1d8d1b0eb2cf86327ec3f7dd1029954f8846b8
Shadi Jiha <shadijiha@gmail.com>
2020-08-02 05:47:31 -0400

    Update Beverage.js

--- src/scripts/Beverage.js
+++ src/scripts/Beverage.js
@@ -1,4 +1,4 @@
-/** 
+/**
  * name:    Name of the object. Used in the page title, the item title, and as the alt image text
  * desc:    Full description of the object.
  * price:   Price of the object
@@ -25,13 +25,14 @@ var qty = 0;
  * is selected.
  */
 function updatePageContents() {
-    document.getElementById("productTitle").innerHTML = name; // Page title
-    document.getElementById("productImg").src = img; // Product image
-    document.getElementById("productImg").alt = name; // Product image alt text
-    document.getElementById("productName").innerHTML = name; // Product title
-    document.getElementById("productPrice").innerHTML = "$" + price + " (In stock!)"; // Product price
-    changeSelectionBtn(currentItem); // Product option buttons
-    displayDesc(); // Product description
+	document.getElementById("productTitle").innerHTML = name; // Page title
+	document.getElementById("productImg").src = img; // Product image
+	document.getElementById("productImg").alt = name; // Product image alt text
+	document.getElementById("productName").innerHTML = name; // Product title
+	document.getElementById("productPrice").innerHTML =
+		"$" + price + " (In stock!)"; // Product price
+	changeSelectionBtn(currentItem); // Product option buttons
+	displayDesc(); // Product description
 }
 
 /**
@@ -39,26 +40,28 @@ function updatePageContents() {
  * expands (or shortens) it when requested.
  */
 function displayDesc() {
-    if (showAll) {
-        document.getElementById("productDesc").innerHTML = desc;
-    } else {
-        document.getElementById("productDesc").innerHTML = Sales.private_string_reduceChars(desc, 41);
-    }
+	if (showAll) {
+		document.getElementById("productDesc").innerHTML = desc;
+	} else {
+		document.getElementById(
+			"productDesc"
+		).innerHTML = Sales.private_string_reduceChars(desc, 41);
+	}
 }
 
 /**
  * This function toggles whether the descroption should be expanded or collapsed.
  */
 function showHideDesc() {
-    if (showAll) {
-        document.getElementById("showDescBtn").innerHTML = "More Description...";
-        showAll = false;
-        displayDesc();
-    } else {
-        document.getElementById("showDescBtn").innerHTML = "Less Description...";
-        showAll = true;
-        displayDesc();
-    }
+	if (showAll) {
+		document.getElementById("showDescBtn").innerHTML = "More Description...";
+		showAll = false;
+		displayDesc();
+	} else {
+		document.getElementById("showDescBtn").innerHTML = "Less Description...";
+		showAll = true;
+		displayDesc();
+	}
 }
 
 /**
@@ -67,19 +70,20 @@ function showHideDesc() {
  * @param {*} productOption The product option that was selected
  */
 function changeSelectionBtn(productOption) {
-    for (let i = 1; i <= options; i++) {
-        document.getElementById("productOption" + i).className = "product_option_btn";
-    }
-    document.getElementById("productOption" + productOption).className = "product_option_btn_selected";
+	for (let i = 1; i <= options; i++) {
+		document.getElementById("productOption" + i).className =
+			"product_option_btn";
+	}
+	document.getElementById("productOption" + productOption).className =
+		"product_option_btn_selected";
 }
-
 
 /**
  * Changes the quantity to add to cart
  * @param {*} newQty Quantity value to use.
  */
 function setQty(newQty) {
-    document.getElementById("productQty").value = newQty;
+	document.getElementById("productQty").value = newQty;
 }
 
 /**
@@ -88,35 +92,38 @@ function setQty(newQty) {
  * @param {*} limit Max amount to allow increasing to
  */
 function updateQty(direction, limit) {
+	qty = document.getElementById("productQty").value;
 
-    qty = document.getElementById("productQty").value;
+	if (qty < 0) {
+		qty = 0;
+		document.getElementById("productQty").value = 0;
+		return;
+	} else if (qty > limit) {
+		alert(
+			"Warning: Maximum item purchase limit exceeded. Reverting to " +
+				limit +
+				"."
+		);
+		qty = limit;
+		document.getElementById("productQty").value = limit;
+		return;
+	}
 
-    if (qty < 0) {
-        qty = 0;
-        document.getElementById("productQty").value = 0;
-        return;
-    } else if (qty > limit) {
-        alert("Warning: Maximum item purchase limit exceeded. Reverting to " + limit + ".");
-        qty = limit;
-        document.getElementById("productQty").value = limit;
-        return;
-    }
-
-    if (direction) {
-        if (qty == limit) {
-            return;
-        } else {
-            qty++;
-            document.getElementById("productQty").value = qty;
-        }
-    } else {
-        if (qty == 0) {
-            return;
-        } else {
-            qty--;
-            document.getElementById("productQty").value = qty;
-        }
-    }
+	if (direction) {
+		if (qty == limit) {
+			return;
+		} else {
+			qty++;
+			document.getElementById("productQty").value = qty;
+		}
+	} else {
+		if (qty == 0) {
+			return;
+		} else {
+			qty--;
+			document.getElementById("productQty").value = qty;
+		}
+	}
 }
 
 /**
@@ -124,18 +131,23 @@ function updateQty(direction, limit) {
  * @param {*} limit The maximum allowable quantity to add to cart.
  */
 function addToCart(limit) {
-    qty = document.getElementById("productQty").value;
+	qty = document.getElementById("productQty").value;
 
-    if (qty <= 0) {
-        alert("You need to indicate a quantity before adding to cart!");
-        return;
-    } else if (qty > limit) {
-        alert("Warning: Maximum item purchase limit exceeded. Reverting to " + limit + ".");
-        qty = limit;
-        document.getElementById("productQty").value = limit;
-        return;
-    } else {
-        for (let i = 0; i < qty; i++)
-            cart.void_add(new Item(42, name, 'Beverages', 'sprite.jpg', price, 1, 0, ''));
-    }
+	if (qty <= 0) {
+		alert("You need to indicate a quantity before adding to cart!");
+		return;
+	} else if (qty > limit) {
+		alert(
+			"Warning: Maximum item purchase limit exceeded. Reverting to " +
+				limit +
+				"."
+		);
+		qty = limit;
+		document.getElementById("productQty").value = limit;
+		return;
+	} else {
+		cart.void_add(
+			new Item(42, name, "Beverages", "sprite.jpg", price, qty, 0, "")
+		);
+	}
 }
